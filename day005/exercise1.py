97050fef0a9e25152375c74f41285ad87af65e87
Ahmad Nizam <mohdrosnizam@gmail.com>
2023-11-11 11:42:13 +0800

    day 005

---
+++ day005/exercise1.py
@@ -0,0 +1,15 @@
+# 🚨 Don't change the code below 👇
+student_heights = input("Input a list of student heights ").split()
+for n in range(0, len(student_heights)):
+  student_heights[n] = int(student_heights[n])
+# 🚨 Don't change the code above 👆
+
+
+#Write your code below this row 👇
+total_height=0
+count = 0
+for h in student_heights:
+    total_height += h
+    count += 1
+avg = round(total_height/count)
+print(avg)
---
+++ day005/main.py
@@ -0,0 +1,4 @@
+fruits = [ "Apple", "Peach", "Pear", "Durian" ]
+for fruit in fruits:
+    print(fruit + " Pie")
+print(fruits)
